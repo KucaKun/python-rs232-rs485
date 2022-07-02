b5a2ee34cc488ee1a453859e17e99478b9d32aae
Adrian Kuca <adriankuca2@gmail.com>
2022-07-02 15:28:54 +0200

    Fix things after testing. Works fine now

--- rs232.py
+++ rs232.py
@@ -11,7 +11,7 @@ def get_serials():
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("port", type=str)
+parser.add_argument("port", type=str, default="loopback")
 parser.add_argument("--baudrate", type=int, default="9600")
 parser.add_argument("--parity", type=str, help="none, odd or even", default="none")
 parser.add_argument("--stopbits", type=int, help="1 or 2 bits", default=1)
@@ -24,11 +24,10 @@ parser.add_argument(
     "--timeout", type=int, help="seconds of response timeout", default=3
 )
 args = parser.parse_args()
-
-if args.port not in get_serials():
-    print("ERROR: No such port")
-    exit(1)
-
+if args.port != "loopback":
+    if args.port not in get_serials():
+        print("ERROR: No such port")
+        exit(1)
 if args.parity == "none":
     parity = serial.PARITY_NONE
 elif args.parity == "odd":
@@ -54,41 +53,57 @@ elif args.format == 8:
 else:
     print("ERROR: Bad format")
     exit(1)
+if args.port != "loopback":
+    ser = serial.Serial(
+        port="COM1",
+        baudrate=args.baudrate,
+        parity=parity,
+        stopbits=stopbits,
+        bytesize=format,
+        timeout=args.timeout,
+        xonxoff=args.xonxoff,
+        rtscts=args.rtscts,
+        dsrdtr=args.dsrdtr,
+    )
+else:
+    ser = serial.serial_for_url(
+        "loop://",
+        baudrate=args.baudrate,
+        parity=parity,
+        stopbits=stopbits,
+        bytesize=format,
+        timeout=args.timeout,
+        xonxoff=args.xonxoff,
+        rtscts=args.rtscts,
+        dsrdtr=args.dsrdtr,
+    )
 
-ser = serial.Serial(
-    port="COM1",
-    baudrate=9600,
-    parity=parity,
-    stopbits=stopbits,
-    bytesize=format,
-    timeout=args.timeout,
-    xonxoff=args.xonxoff,
-    rtscts=args.rtscts,
-    dsrdtr=args.dsrdtr,
-)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser), newline=args.terminator)
+last_recv = time.time()
 
-last_recv = 0
+
+def send(text):
+    ser.write((text + args.terminator).encode("utf-8"))
 
 
 def recieve_loop():
     global last_recv
     while True:
-        try:
-            print("Recieved:", sio.readline())
+        recv = ser.read_until(args.terminator.encode("utf-8")).decode("utf-8")
+        if recv:
+            print("\t\tRecieved:", recv)
             last_recv = time.time()
-        except:
-            pass
 
 
 def measure_time():
     previous_last_recv = last_recv
-    sio.write(inp + args.terminator)
+    send("ping")
     while last_recv <= previous_last_recv:
         roundtrip = last_recv - previous_last_recv
         if time.time() - previous_last_recv > args.timeout:
             print("ERROR: Timeout while trying to calculate roundtrip.")
-            break
+            return
+    send("pong")
+    time.sleep(0.2)
     print("Roundtrip is:", roundtrip, "seconds")
 
 
@@ -97,12 +112,12 @@ t.daemon = True
 t.start()
 
 while True:
-    inp = input()
+
+    inp = input("Command: ")
     if inp == "ping":
         measure_time()
     elif inp == "exit":
         exit(0)
     else:
-        print("Sending:", inp)
-        sio.write(inp + args.terminator)
-        print("")
+        send(inp)
+        time.sleep(0.2)
